d5c7cf20ea5239f302306be6896cecee067f62ea
rahulengg136 <rahulengg136@gmail.com>
2021-10-03 16:47:54 +0530

    Changed slight structuring

---
+++ AdFormsAssignment/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AdFormAssignment.DAL.Entities;
+using AdFormsAssignment.DTO;
+using AutoMapper;
+
+namespace AdFormsAssignment.AutoMapper
+{
+    /// <summary>
+    /// Automapper profile
+    /// </summary>
+    public class AutoMapperProfile : Profile
+    {
+        /// <summary>
+        /// All required mapping configurations
+        /// </summary>
+        public AutoMapperProfile()
+        {
+            CreateMap<TodoListDto, tblTodoList>();
+            CreateMap<TodoItemDto, tblTodoItem>();
+            CreateMap<LabelDto, tblLabel>();
+        }
+    }
+}
